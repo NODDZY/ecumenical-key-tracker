6e1f44a9c1861c5655c208ce52e4b42e702c0347
matssg <40918636+NODDZY@users.noreply.github.com>
2023-12-30 22:27:24 +0100

    Count and store key amount

--- src/main/java/com/keytracker/EcumenicalPlugin.java
+++ src/main/java/com/keytracker/EcumenicalPlugin.java
@@ -1,9 +1,12 @@
 package com.keytracker;
 
 import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.api.VarPlayer;
+import net.runelite.api.*;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -16,11 +19,16 @@ import static net.runelite.api.Varbits.DIARY_WILDERNESS_HARD;
 	name = "Ecumenical Key Tracker"
 )
 public class EcumenicalPlugin extends Plugin {
+	public static final String CONFIG_GROUP_NAME = "ecumenical-key-tracker";
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private OverlayManager overlayManager;
+
+	@Inject
+	private ConfigManager configManager;
 
 	@Inject
 	private EcumenicalOverlay overlay;
@@ -35,17 +43,33 @@ public class EcumenicalPlugin extends Plugin {
 		overlayManager.remove(overlay);
 	}
 
-	private int getEcumenicalKeyCount() {
-		int varPlayerId = VarPlayer.BIRD_HOUSE_MEADOW_NORTH; // ECUMENICAL_KEY_COUNT.getId();
-
-		return client.getVarbitValue(varPlayerId);
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event) {
+		if (event.getContainerId() == InventoryID.INVENTORY.getId() || event.getContainerId() == InventoryID.BANK.getId()) {
+			// Check for ecumenical keys in bank or inventory
+			Item[] items = event.getItemContainer().getItems();
+			int tempKeyCount = 0;
+			for (Item item : items) {
+				if (item.getId() == ItemID.ECUMENICAL_KEY) {
+					tempKeyCount += item.getQuantity();
+				}
+			}
+			// Store key count
+			if (event.getContainerId() == InventoryID.INVENTORY.getId())  {
+				log.debug("Keys in inventory: " + tempKeyCount);
+				configManager.setRSProfileConfiguration(CONFIG_GROUP_NAME, "ecumenicalKeyCountInventory", tempKeyCount);
+			} else {
+				log.debug("Keys in bank: " + tempKeyCount);
+				configManager.setRSProfileConfiguration(CONFIG_GROUP_NAME, "ecumenicalKeyCountBank", tempKeyCount);
+			}
+		}
 	}
 
 	public String generateOverlayText() {
 		StringBuilder message = new StringBuilder();
-		// Current key count
-		// TODO Get amount of keys stored
-		message.append("T");
+		// Get current key count
+		int totalKeyCount = Integer.parseInt(configManager.getRSProfileConfiguration(CONFIG_GROUP_NAME, "ecumenicalKeyCountInventory")) + Integer.parseInt(configManager.getRSProfileConfiguration(CONFIG_GROUP_NAME, "ecumenicalKeyCountBank"));
+		message.append(totalKeyCount);
 
 		// Max key limit
 		int maxAmount = 3;
